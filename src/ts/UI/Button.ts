0dcfb1a20dcec877aa63a3a366e98aa873734e61
Phree <phreework@gmail.com>
2021-07-07 22:54:53 +0800

    按钮组件更新 -定义按钮接口事件 -定义按钮视觉模式 -定义按钮多项属性

--- src/ts/UI/Button.ts
+++ src/ts/UI/Button.ts
@@ -1,28 +1,63 @@
-import { EventDisPatcher, Sprite, SpriteFrame } from "../Core";
+import { Entity, EventDisPatcher, Sprite, SpriteFrame } from "../Core";
 import { Debug } from "../Debug";
 import { rClass } from "../Decorator";
 import { UIComponent } from "./UIComponent";
 
+/**
+ * @interface 按钮事件接口 
+ */
+interface IButtonEvent {
+    target: Entity;
+    scriptName: string;
+    funcName: string;
+    params: any[];
+}
+/**
+ * @enum 按钮视觉模式，1none（不使用变换），2Sprite（使用多张精灵图），3scale（自动缩放）
+ */
+enum ButtonTransMode {
+    none = 1,
+    sprite = 2,
+    scale = 3
+}
+/**
+ * @class 按钮组件
+ */
 @rClass
-export class Button extends UIComponent{
+export class Button extends UIComponent {
     /**
-     * defaultSprite
+     * targetEntity
      */
-    defaultImage:SpriteFrame;
+    target: Entity;
     /**
-     * pressedSprite
+     * defaultImage
      */
-    pressedImage:SpriteFrame;
+    defaultImage: SpriteFrame;
     /**
-     * 事件
+     * pressedImage
      */
-    event:EventDisPatcher;
-    onLoad(){
-        try{
-            const spr = this.entity.getComponent(Sprite);
-            spr.spriteFrame = this.defaultImage;
-        }catch(e){
-            Debug.error(e,"按钮可能没有实体，或实体上没有Sprite组件");
+    pressedImage: SpriteFrame;
+    /**
+     * hoverImage
+     */
+    hoverImage: SpriteFrame;
+    /**
+     * disableImage
+     */
+    disableImage: SpriteFrame;
+    /**
+     * 变换模式：1none（不使用变换），2Sprite（使用多张精灵图），3scale（自动缩放）
+     */
+    mode: ButtonTransMode = ButtonTransMode.none;
+    /**
+     * 事件组  
+     */
+    events: IButtonEvent[];
+    onLoad() {
+        try {
+            if (this.entity)this.target = this.entity;
+        } catch (e) {
+            Debug.error(e, "按钮可能没有实体");
         }
     }
 }
